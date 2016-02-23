f905793425168643a0ea00da79a7d4eca9bf5831
Shishir Kumar. Mishra <26197665@WUSPEEDPAY.COM>
2016-02-23 08:10:36 -0500

    sample change

--- teamcity/HelloWorld/HelloWorld/Global.asax.cs
+++ teamcity/HelloWorld/HelloWorld/Global.asax.cs
@@ -32,7 +32,6 @@ namespace HelloWorld
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
         }
