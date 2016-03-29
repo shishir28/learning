23f1e64d1cf3179f260876cf797f964ede5bca5a
Shishir Kumar Mishra <shishir28@gmail.com>
2016-03-29 21:56:14 +0530

    .net producer has been aligned to nodejs codebase

---
+++ RabbitMQ/src/DotNet/Producer/Email.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+
+    [Serializable]
+    public class EmailPerson
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string address { get; set; }
+    }
+
+    [Serializable]
+    public class Email
+    {
+        public string subject { get; set; }
+        public string body { get; set; }
+        public EmailPerson from { get; set; }
+        public List<EmailPerson> toList { get; set; }
+    }
+
+}
--- RabbitMQ/src/DotNet/Producer/Program.cs
+++ RabbitMQ/src/DotNet/Producer/Program.cs
@@ -1,12 +1,15 @@
 ﻿using RabbitMQ.Client;
 using System;
+using Newtonsoft.Json;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-
 namespace Producer
 {
     public class Program
     {
+       
         public static void Main(string[] args)
         {
             var factory = new ConnectionFactory() { HostName = "localhost" };
@@ -14,13 +17,22 @@ namespace Producer
             using (var ch = conn.CreateModel())
             {
                 ch.ExchangeDeclare(exchange: "sampleTopic", type: "topic", durable: true);
-                var key = (args.Length > 0) ? args[0] : "dummyKey";               
+                var key = (args.Length > 0) ? args[0] : "dummyKey";
 
-                var messageTobeSent = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello Shishir from .NET";
+                // var messageTobeSent = (args.Length > 1) ? string.Join(" ", args.Skip(1).ToArray()) : "Hello Shishir from .NET";
+
+                var messageTobeSent = new Email
+                {
+                    body = "Hello .Net  World",
+                    subject = "Hello  .NET Message",
+                    from = new EmailPerson { firstName = "JohnDot", lastName = "DoeNET", address = "shishir28@gmail.com" },
+                    toList = new System.Collections.Generic.List<EmailPerson> { new EmailPerson { firstName = "JaneDot", lastName = "DoeNET", address = "shishir28@live.com" } }
+                };
+
                 ch.BasicPublish(exchange: "sampleTopic",
                                     routingKey: key,
                                     basicProperties: null,
-                                    body: Encoding.UTF8.GetBytes(messageTobeSent));
+                                    body: Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(messageTobeSent)));
                 Console.WriteLine(" [x] Sent message with [0] and content [1]", key, messageTobeSent);
             }
         }
